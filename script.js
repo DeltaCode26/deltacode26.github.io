fc4b5bb51b88a24d509580283d6fb3dbdaf61772
DeltaCode26 <141487027+DeltaCode26@users.noreply.github.com>
2023-09-16 12:23:33 +0530

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-function myFunction() {
-  let x = document.getElementById("links");
-  if (x.classList.contains("show")) {
-    x.classList.remove("show", "overlay");
-  } else {
-    x.classList.add("show", "overlay");
-  }
-}
